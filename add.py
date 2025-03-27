3f0be86f4487334e0b140f13bab1f5b5ea3b3868
SakshiJadhav173 <jjadhav.sakshii02@gmail.com>
2025-03-27 15:18:19 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,6 +1,7 @@
-x = input("Type a number: ")
+px = input("Type a number: ")
 y = input("Type another number: ")
 
 sum = int(x) + int(y)
 
-print("The sum is: ", sum) 
+print("The sum is: ", sum)
+print("Successful")
